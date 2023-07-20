2c50133ed58b4ba014492c6377f7dee352f00756
Matthew Pombo <datacorruption10@gmail.com>
2023-07-20 10:52:32 -0700

    fixed-update-script

--- update.lua
+++ update.lua
@@ -2,6 +2,6 @@
 
 local url, f, p = "https://data-corruption.github.io/CC-Scripts/Programs/", fs.open("programs.txt", "r"), {}
 shell.run("wget", url .. "programs.txt", "programs.txt")
-for l in f.readLine do table.insert(p, l) end; f.close()
-for _, pr in ipairs(p) do print("Updating " .. pr .. "..."); shell.run("rm", pr); shell.run("wget", url .. pr, pr) end
+if f then for l in f.readLine do table.insert(p, l) end; f.close() end
+for _, pr in ipairs(p) do shell.run("rm", pr); shell.run("wget", url .. pr, pr) end
 print("All scripts updated!")
